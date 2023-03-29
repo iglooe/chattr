a9ccd08db8dd429bab242aa25abe4ccf304bd690
Derek Day <day.derek7@protonmail.com>
2023-03-28 23:40:45 -0500

    added profiles pages with ssg helpers

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,7 +2,6 @@ import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
 
-// type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
     username,
@@ -15,9 +14,22 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
       <Head>
         <title>{data.username}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>{data.username}</div>
-      </main>
+      <PageLayout>
+        <div className="relative h-36  bg-slate-600">
+          <img
+            src={data.profileImageUrl}
+            alt={`${data.username}'s profile image`}
+            width={128}
+            height={128}
+            className="absolute left-4 bottom-0 -mb-[64px] ml-4 rounded-full border-2 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400" />
+      </PageLayout>
     </>
   );
 };
@@ -25,6 +37,7 @@ import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createProxySSGHelpers({
